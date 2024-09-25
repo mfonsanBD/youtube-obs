517c79c405651f4c1c7e13b59d3bea9d8291a90f
Mike Santos <iddeiasimplesmike@gmail.com>
2024-09-25 20:24:20 -0300

    monitoring stream and broadcast status to turn broadcast live

--- app/helpers/youtube.ts
+++ app/helpers/youtube.ts
@@ -4,14 +4,21 @@ import { env } from '../types/env';
 import { connectOBS, setStreamKey, startStream } from './obs';
 const youtube = google.youtube('v3');
 
-export async function createBroadcast(accessToken: string, title: string, description: string) {
-  const auth = new google.auth.OAuth2({
+let broadcastStatusData: string = 'created'
+let streamStatusData: string = 'inactive'
+let streamHealthStatusData: string = 'noData'
+
+export async function createGoogleAuth() {
+  return new google.auth.OAuth2({
     clientId: env.CLIENT_ID,
     clientSecret: env.CLIENT_SECRET,
     redirectUri: 'http://localhost:3000/callback',
   });
+}
 
-  auth.setCredentials({ access_token: accessToken, scope: 'https://www.googleapis.com/auth/youtube.force-ssl' });
+export async function createBroadcast(accessToken: string, title: string, description: string) {
+  const auth = await createGoogleAuth()
+  auth.setCredentials({ access_token: accessToken, scope: 'https://www.googleapis.com/auth/youtube.force-ssl' })
 
   // Cria uma transmissão
   const broadcast = await youtube.liveBroadcasts.insert({
@@ -60,13 +67,66 @@ export async function createBroadcast(accessToken: string, title: string, descri
   await setStreamKey(stream.data.cdn?.ingestionInfo?.streamName as string); // Configurar a Stream Key do YouTube
   await startStream(); // Iniciar a transmissão no OBS
 
-  await new Promise(() => setTimeout(() => {
-    youtube.liveBroadcasts.transition({
+  const checkStatusInterval = setInterval(async () => {
+    const broadcastStatus = await youtube.liveBroadcasts.list({
       auth,
-      broadcastStatus: 'live',
-      id: broadcast.data.id as string,
+      id: [broadcast.data.id as string],
+      part: ['status'],
+    });
+  
+    const streamStatus = await youtube.liveStreams.list({
+      auth,
+      id: [stream.data.id as string],
+      part: ['status'],
+    });
+  
+    broadcastStatusData = broadcastStatus!.data!.items![0].status?.lifeCycleStatus as string;
+    streamStatusData = streamStatus!.data!.items![0].status?.streamStatus as string;
+    streamHealthStatusData = streamStatus!.data!.items![0].status?.healthStatus?.status as string;
+  
+    console.log('Broadcast Status:', broadcastStatusData);
+    console.log('Stream Status:', streamStatusData);
+    console.log('Stream Health Status:', streamHealthStatusData);
+  
+    // Se tudo estiver em ordem, faça a transição para "live"
+    if (
+      broadcastStatusData === 'testing' &&
+      streamStatusData === 'active' &&
+      streamHealthStatusData === 'good'
+    ) {
+      try {
+        await youtube.liveBroadcasts.transition({
+          auth,
+          broadcastStatus: 'live',
+          id: broadcast.data.id as string,
+          part: ['id', 'snippet', 'status'],
+        });
+        console.log('Transmissão ao vivo iniciada!');
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        console.error('Erro ao fazer a transição:');
+      }
+
+      clearInterval(checkStatusInterval); // Pare o intervalo após a transição bem-sucedida
+    }
+  }, 5000)
+}
+
+export async function endBroadcast(accessToken: string, broadcastId: string) {
+  const auth = await createGoogleAuth()
+  auth.setCredentials({ access_token: accessToken, scope: 'https://www.googleapis.com/auth/youtube.force-ssl' })
+
+  try {
+    // Transicionar a transmissão para "complete"
+    await youtube.liveBroadcasts.transition({
+      auth,
+      broadcastStatus: 'complete',
+      id: broadcastId,
       part: ['id', 'snippet', 'status'],
-    })
-  }, 5 * 60 * 1000))
+    });
+    console.log('Transmissão encerrada com sucesso!');
+  } catch (error) {
+    console.error('Erro ao encerrar a transmissão:', error);
+  }
 }
 
